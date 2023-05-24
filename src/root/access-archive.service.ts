c8435c66706472be510af2258596555c5750ff33
LucaPoggianti <43633746+LucaPoggianti@users.noreply.github.com>
2023-05-24 10:19:20 +0200

    AccessArchiveService added and completed

---
+++ src/root/access-archive.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ajax, AjaxResponse } from 'rxjs/ajax';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AccessArchiveService {
+  base: string = 'https://eu-central-1.aws.data.mongodb-api.com/app/kvaas-giwjg/endpoint/';
+  key: string = '8b1dd28c';
+  
+  public getArchive(): Observable<AjaxResponse<any>> {
+    return ajax({
+      method: 'GET',
+      url: this.base + 'get?key=' + this.key,
+      crossDomain: true
+    });    
+  }
+
+  public saveArchive(newArchive:string): Observable<AjaxResponse<any>> {
+    return ajax({
+      method: 'POST',
+      url: this.base + 'set?key=' + this.key,
+      crossDomain: true,
+      body: newArchive
+    });
+  }
+
+  constructor() {}
+
+}
