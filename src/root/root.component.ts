c60cfd63a24f34f39ef73e840f38e57ec23e47f9
LucaPoggianti <43633746+LucaPoggianti@users.noreply.github.com>
2023-05-24 10:22:49 +0200

    TitleComponent added and completed

--- src/root/root.component.ts
+++ src/root/root.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TitleComponent } from './title/title.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './root.component.html',
   styleUrls: ['./root.component.css'],
   standalone: true,
+  imports: [CommonModule, TitleComponent]
 })
 
 export class RootComponent implements OnInit {
+  status: string = 'home';
 
   constructor() {}
 
