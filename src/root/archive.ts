f4350657f5f33c00bc14d71348cd9a5f402494a4
LucaPoggianti <43633746+LucaPoggianti@users.noreply.github.com>
2023-05-24 10:17:02 +0200

    Archive class added

---
+++ src/root/archive.ts
@@ -0,0 +1,12 @@
+import { Book } from './book';
+
+export class Archive {
+  elenco: Array<Book>;
+  constructor(elenco:Array<Book>) {
+    this.elenco = elenco;
+  }
+  checkKey(key:string) {
+    let result: Array<Book> = this.elenco.filter((book) => book.title.concat(' ', book.author).toLowerCase().includes(key.toLowerCase()));
+    return result;
+  }
+}
